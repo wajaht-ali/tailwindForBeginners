612dfe8001c5ac5cc338518fb2eeaaec86f0861c
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-11-01 13:13:32 +0500

    basic text fonts and colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-
+import './styles/styles.css';
 function App() {
   return (
     <div className="App">
-      <h2 className="text-white bg-slate-900">Tailwind with React</h2>
+      <h2 className="text-center bg-slate-400 text-white p-3">Testing Tailwind App</h2>
     </div>
   )
 }
